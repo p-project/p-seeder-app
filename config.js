f8363b5bda6cf573da4c3d97975f254f4da5e8b4
Adrien Gregorj <adrien.gregorj@gmail.com>
2016-11-27 16:47:37 +0100

    .gitignore is now working

--- config.js
+++ config.js
@@ -3,15 +3,10 @@
 const path = require('path')
 
 let config = {
-  // Name of electron app
-  // Will be used in production builds
   name: 'p-seeder',
 
-  // webpack-dev-server port
   port: 9080,
 
-  // electron-packager options
-  // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
   building: {
     arch: 'x64',
     asar: true,
