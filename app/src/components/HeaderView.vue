053639e980f125a4dcc145161e418eb069b0d54a
Adrien Gregorj <adrien.gregorj@gmail.com>
2016-12-02 18:47:00 +0100

    change menu

--- app/src/components/HeaderView.vue
+++ app/src/components/HeaderView.vue
@@ -1,27 +1,31 @@
 <template>
     <header>
         <div class="bar">
-            <div class="home-icon">
-                <router-link to="/">
-                    <md-button class="md-icon-button md-raised md-primary">
-                        <md-icon>home</md-icon>
-                    </md-button>
-                </router-link>
-            </div>
+            <md-menu>
+                <md-button class="md-icon-button md-raised md-primary" md-menu-trigger>
+                    <md-icon>more_vert</md-icon>
+                </md-button>
+
+                <md-menu-content>
+                        <md-menu-item>
+                            <md-icon>home</md-icon>
+                        </md-menu-item>
+                    <md-menu-item>My Item 2</md-menu-item>
+                    <md-menu-item>My Item 3</md-menu-item>
+                </md-menu-content>
+            </md-menu>
         </div>
     </header>
 </template>
 
 <style scoped lang="scss">
     header {
-        height: 50px;
+        padding-top: 10px;
         width: 100%;
         top: 0;
         left: 0;
     }
-    .bar {
-        margin-top: 10px;
-    }
+
 </style>
 
 <script>
