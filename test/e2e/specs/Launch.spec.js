1ad9e81cae79aecc8e96ae163ed0828a4afde4a6
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2017-03-27 22:05:58 +0200

    add test for number of window

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -4,6 +4,13 @@ describe('Launch', function () {
   beforeEach(utils.beforeEach)
   afterEach(utils.afterEach)
 
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount()
+      .then(count => {
+        expect(count).to.equal(1)
+    })
+  })
+
   it('shows the proper application title', function () {
     return this.app.client.getTitle()
       .then(title => {
