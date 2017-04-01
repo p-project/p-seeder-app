9f199368183d4a731020ad58fefb5ce490605be9
Quentin de Longraye <quentin@dldl.fr>
2017-04-01 12:25:05 +0200

    try to add basic tests for homepage

--- test/e2e/specs/Home.spec.js
+++ test/e2e/specs/Home.spec.js
@@ -1,3 +1,20 @@
-/**
- * Created by buubbly on 27/03/2017.
- */
+import utils from '../utils'
+
+describe('Homepage', function () {
+    beforeEach(utils.beforeEach)
+    afterEach(utils.afterEach)
+
+    it('should display a welcoming title', function () {
+        return this.app.client.getTagName('h1')
+            .then(text => {
+                expect(text).to.equal("Bienvenue.")
+            })
+    })
+
+    it('shows display a login form', function () {
+        let elem = $('.login-form')
+        let tagName = elem.getTagName()
+
+        return expect(tagName).to.equal('form')
+    })
+})
