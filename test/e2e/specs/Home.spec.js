3cc795e31e64842cba8d66ec5178cd14f9598e5f
Quentin de Longraye <quentin@dldl.fr>
2017-05-25 16:01:00 +0200

    postinstall changes

--- test/e2e/specs/Home.spec.js
+++ test/e2e/specs/Home.spec.js
@@ -1,19 +1,19 @@
 import utils from '../utils'
 
 describe('Homepage', function () {
-    beforeEach(utils.beforeEach)
-    afterEach(utils.afterEach)
+  beforeEach(utils.beforeEach)
+  afterEach(utils.afterEach)
 
-    it('should display a welcoming title', function () {
-        return this.app.client.getText('h1')
+  it('should display a welcoming title', function () {
+    return this.app.client.getText('h1')
             .then(text => {
-                expect(text).to.equal("Welcome.")
+              expect(text).to.equal('Welcome.')
             })
-    })
+  })
 
-    it('shows a login form', function () {
-        let form = this.app.client.element('form')
+  it('shows a login form', function () {
+    let form = this.app.client.element('form')
 
-        return expect(form).to.not.equal(undefined)
-    })
+    return expect(form).to.not.equal(undefined)
+  })
 })
